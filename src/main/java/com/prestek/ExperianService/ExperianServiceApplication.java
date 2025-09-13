b36ad15bc78f2636c597dc6f332ce8687779188c
JVARGAS <jaider.vargas-n@mail.escuelaing.edu.co>
2025-09-13 11:16:57 -0500

    initial commit

---
+++ src/main/java/com/prestek/ExperianService/ExperianServiceApplication.java
@@ -0,0 +1,13 @@
+package com.prestek.ExperianService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExperianServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExperianServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prestek/ExperianService/ExperianServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prestek.ExperianService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExperianServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
